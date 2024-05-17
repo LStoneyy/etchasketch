22449fac78f87fc8a322866db80b0412a1a376a2
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2024-05-17 17:07:33 +0200

    Added full grid functionality

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 containerDiv = document.querySelector(".container");
+let grid = 16
+createGrid(grid)
 
-for (let rindex = 0; rindex < 16; rindex++) {
-    let rows = document.createElement("div");
+function changeGrid() {
+    let gridwidth = prompt("How wide should the Grid be?");
+    let grid = parseInt(gridwidth);
 
-    for (let cindex = 0; cindex < 16; cindex++) {
-        let columns = document.createElement("div");
-        rows.append(columns);
+    if (grid > 100) {
+        grid = parseInt(prompt("Error! Number must be below 100. Enter a new one:"))
+    } else {
+        containerDiv.textContent="";
+        createGrid(grid);
     }
-
-    containerDiv.append(rows);
 }
+
+function createGrid(grid) {
+    for (let rindex = 0; rindex < grid; rindex++) {
+        let rows = document.createElement("div");
+    
+        for (let cindex = 0; cindex < grid; cindex++) {
+            let columns = document.createElement("div");
+            columns.textContent = "Test";
+            columns.addEventListener("mouseover", hovering, true)
+            rows.append(columns);
+    
+            function hovering() {
+                columns.setAttribute("style", "color: blue")
+            }
+        }
+    
+        containerDiv.append(rows);
+    }
+}
+
 
 
